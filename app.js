e5586ae6a725e5354b0a3113e3a7cb614c07efcd
YasirTechCode <yasirtechcode@gmail.com>
2024-12-18 12:07:59 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,121 @@
+
+// Chapter 01 
+// Chapter 01
+// Question 01
+
+// Answer
+
+ alert("Welcome To \n Our Website");
+
+
+// Question 02
+// Answer
+
+// ===============> alert("Error! Please Enter a valid Password");
+
+// Quesion 03
+// Answer
+
+// ===============> alert("Welocme to Js Land... \n Happy Coding");
+
+// Quesion 04
+// Answer
+
+// ===============> alert("Welocme to Js Land...");
+
+// Question 05
+// Answer
+
+// ===============> alert("Happy Coding");
+
+// Question 06
+// Answer
+
+// ===============> alert("Hello... I can run JS through my web browser's console");
+                                    //Chapter 02 
+
+// Chapter 02
+
+// Question 01
+// Answer
+
+// ===============> var username = Jhon Deo;
+
+// Question 02
+// Answer
+
+// ===============> var myName = Muhammad Huzaifa Khan;
+
+// Question 03
+// Answer
+
+// (A) ===============> var titled;
+// (B) ===============> titled = "Hello World";
+// (C) ===============> alert(titled);
+
+// Question 04
+// Answer
+// ===============> var name = Jhon Deo;
+// ===============> var age = 18;
+// ===============> var course = "Certified ";
+// ===============> alert(name);
+// ===============> alert(age);
+// ===============> alert(course);
+
+// Question 05
+// Answer
+// ===============> var a = "PIZZA";
+// ===============> alert(a);
+
+// Question 06
+// Answer
+// ===============> var email = "yasirali27@gmail.com";
+// ===============> alert(My email address is ${email}.);
+
+
+// Question 07
+// Answer
+// ===============> var book = "A Smarter Way to Learn JavaScript";
+// ===============> alert(I am trying to learn from the book "${book}".);
+
+// Question 08
+// Answer
+// ===============> var content = "Yah! I can write HTML Content Throgh JavaScript";
+// ===============> document.write(content);
+
+// Question 09
+// Answer
+// ===============> var content = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// ===============> alert(content);
+
+                                       //  Chapter 03 
+
+// Chapter 03
+
+// Question 01
+// Answer
+// ===============> var age = 20;
+// ===============> alert(I am ${age} years old.);   
+
+// Question 02
+// Answer
+
+// ===============> var number = 10;
+// ===============> alert(You have visited this site ${number} times);
+
+// Question 03
+// Answer
+//===============> var birthYear = 2006;
+// ===============> document.write(My birth Year is ${birthYear} <br> Data type of my declared variale is ${typeof(birthYear)});
+
+// Quesion 04
+// Answer
+
+// (A) ===============> var visitorName = "Yasir Ali";
+// (B) ===============> var producttitle = "Wrist Watch";
+// (C) ===============> var quantity = 3;
+
+// ===============> document.write(<b> ${visitorName} </b> Orderd <b> ${quantity} ${producttitle} </b> on Nasa.pk);
+
+
+// Assignment01 end
